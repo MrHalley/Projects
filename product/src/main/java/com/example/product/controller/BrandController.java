d8d7c9ae77ea4b51cc77925c3933143d471bcc79
Mr.Du laptop <1165449791@qq.com>
2022-06-09 23:29:13 +0800

    JSR303校验
    1.基本校验配合@Valid（@Validated）、BindingResult进行校验
    2.通过统一异常处理@ControllerAdvice MethodArgumentNotValidException进行统一返回校验的错误结果
    3.分组校验@Validated({AddGroup.class}),只校验有对应分组标识的校验字段。例：@NotBlank()不校验。同理@Validated()只校验没有分组标识的字段。
    4.自定义校验

--- product/src/main/java/com/example/product/controller/BrandController.java
+++ product/src/main/java/com/example/product/controller/BrandController.java
@@ -1,9 +1,12 @@
 package com.example.product.controller;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +18,7 @@ import com.example.product.service.BrandService;
 import com.example.common.utils.PageUtils;
 import com.example.common.utils.R;
 
+import javax.validation.Valid;
 
 
 /**
@@ -55,9 +59,22 @@ public class BrandController {
      * 保存
      */
     @RequestMapping("/save")
-    public R save(@RequestBody BrandEntity brand){
-		brandService.save(brand);
-
+    public R save(@Validated @RequestBody BrandEntity brand/*, BindingResult result*/){
+//        if(result.hasErrors()){
+//            Map<String,String> map = new HashMap<>();
+//            //1、获取校验的错误结果
+//            result.getFieldErrors().forEach((item)->{
+//                //FieldError 获取到错误提示
+//                String message = item.getDefaultMessage();
+//                //获取错误的属性的名字
+//                String field = item.getField();
+//                map.put(field,message);
+//            });
+//            return R.error(400,"提交的数据不合法").put("data",map);
+//        }else {
+//            brandService.save(brand);
+//        }
+        brandService.save(brand);
         return R.ok();
     }
 

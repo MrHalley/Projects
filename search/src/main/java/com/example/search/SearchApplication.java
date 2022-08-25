f32f73c49cf3e16327d5703652f344ec79b37fc6
MrHalley <38929127+MrHalley@users.noreply.github.com>
2022-08-25 14:18:14 +0800

    Es检索

--- search/src/main/java/com/example/search/SearchApplication.java
+++ search/src/main/java/com/example/search/SearchApplication.java
@@ -3,7 +3,9 @@ package com.example.search;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients(basePackages = "com.example.search.feign")
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SearchApplication {
 
--- search/src/main/java/com/example/search/controller/SearchController.java
+++ search/src/main/java/com/example/search/controller/SearchController.java
@@ -1,18 +1,38 @@
 package com.example.search.controller;
 
+import com.example.search.service.MallSearchService;
+import com.example.search.vo.SearchParam;
+import com.example.search.vo.SearchResult;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author 杜延文
  * @version 1.0
  * @date 2022/8/24 15:30
  */
+@Slf4j
 @Controller
 public class SearchController {
 
+    @Resource
+    private MallSearchService mallSearchService;
+
     @GetMapping(value = {"/","/search.html"})
-    public String search(){
+    public String listPage(SearchParam param, Model model, HttpServletRequest request) {
+
+        param.set_queryString(request.getQueryString());
+
+        //1、根据传递来的页面的查询参数，去es中检索商品
+        SearchResult result = mallSearchService.search(param);
+
+        log.info("result={}",result);
+        model.addAttribute("result",result);
         return "search";
     }
 }
